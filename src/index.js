aeb18c03b6c19ea2fa93c22f3b178b4fca03e7b6
Susan Dreher <sdreher@columbia.edu>
2017-06-07 16:25:11 -0400

    resolve webpack 2 syntax issues & upgrade dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /* global jQuery: true */
 
-require('!file?name=[name].[ext]!../static/index.html');
+require('!file-loader?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('!style!css!bootstrap/dist/css/bootstrap.min.css');
-require('!style!css!bootstrap-arrow-buttons/dist/css/' +
+require('!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('!style-loader!css-loader!bootstrap-arrow-buttons/dist/css/' +
         'bootstrap-arrow-buttons.css');
-require('../static/css/common.css');
-require('../static/css/steps.css');
-require('../static/css/ruralhealth.css');
+require('!style-loader!css-loader!../static/css/common.css');
+require('!style-loader!css-loader!../static/css/steps.css');
+require('!style-loader!css-loader!../static/css/ruralhealth.css');
 
 var jQuery = require('jquery');
 var module = require('./ruralhealth.js');
